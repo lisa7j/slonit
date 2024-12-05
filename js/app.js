8032de78929ab1fe863284743bf9ec7632098c3c
Lisa Sokolskaa <sokolskaaliza@gmail.com>
2024-12-05 22:24:10 +0400

    Переименовала папку js  и добавила мини-игру: Секретное число

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
-function sayHi() {
-    console.log("Hello");
+
+let min = 1;
+let max = 100;
+
+let firstNumber = Math.floor(Math.random() * (max - min)) + min;
+
+console.log(`Компьютер загадал число: ${firstNumber}`);
+console.log('');
+
+
+function SearchNumber(firstNumber, min, max) {
+
+    while (min <= max) {
+        let secondNumber = Math.floor((min + max) / 2);
+        console.log(`Пробую число ${secondNumber}`);
+        
+        if (secondNumber < firstNumber) {
+            console.log('Больше');
+            console.log('');
+            min = secondNumber + 1;
+        } else if (secondNumber > firstNumber) {
+            console.log('Меньше');
+            console.log('');
+            max = secondNumber - 1; 
+        } else {
+            console.log("Угадал");
+            return secondNumber;
+        }
+    }
 }
 
-sayHi();
+SearchNumber(firstNumber, min, max);
